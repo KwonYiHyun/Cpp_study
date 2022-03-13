0b4ddaa061aaa467db711ccef1cd17c56cd68cad
KwonYiHyun <diamoboy1@gmail.com>
2022-03-13 20:25:43 +0900

    inheritance

--- operator overloading/time.h
+++ operator overloading/time.h
@@ -29,6 +29,27 @@ public:
     friend std::ostream& operator<<(std::ostream&, Time&);
 };
 
+/*
+상속
+1. 기존의 클래스에 새로운 기능을 추가할 수 있다.
+2. 클래스가 나타내고 있는 데이터에 다른 것을 더 추가할 수 있다.
+3. 클래스 메소드가 동작하는 방식을 변경할 수 있다.
+*/
+class NewTime : public Time{
+    /*
+    1. 파생 클래스형의 객체 안에는 기초 클래스형의 데이터 멤버들이 저장된다.
+    2. 파생 클래스형의 객체는 기초 클래스형의 메소드들을 사용할 수 있다.
+    3. 파생 클래스는 자기 자신의 생성자를 필요로 한다.
+    4. 파생 클래스는 부가적인 데이터 멤버들과 멤버 함수들을 임의로 추가할 수 있다.
+    */
+private:
+    int day;
+public:
+    NewTime();
+    NewTime(int, int, int);
+    void print();
+};
+
 Time::Time(){
     hours=mins=0;
 }
@@ -77,6 +98,19 @@ Time Time::operator*(int n){
 std::ostream& operator<<(std::ostream& os, Time& t){
     os<<t.hours<<"시간"<<t.mins<<"분";
     return os;
+}
+
+NewTime::NewTime() : Time(){
+    day=0;
+}
+
+NewTime::NewTime(int h, int m, int d) : Time(h,m){
+    day=d;
+}
+
+void NewTime::print(){
+    cout<<"일 : "<<day<<endl;
+    show();
 }
 
 #endif
